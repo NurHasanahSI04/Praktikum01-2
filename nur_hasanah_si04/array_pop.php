8b1cbf13dd43e00a37ee2ff8eefdb8853ae5e517
NurHasanahSI04 <147407769+NurHasanahSI04@users.noreply.github.com>
2024-03-06 13:25:39 +0700

    Add files via upload

---
+++ nur_hasanah_si04/array_pop.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<?php
+    $tims = ["erwin","heru","ali","zaki"];
+    array_pop($tims);
+    foreach($tims as $person){
+        echo $person.'<br/>';
+    }
+?>
+</body>
+</html>
